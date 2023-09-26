023a6dba8d48a6aae46e75bfbc4b14e0c0d0aa69
jeevanandham-381 <jeevanandham381@gmail.com>
2023-09-26 15:41:25 +0530

    sec

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mymodule
+
+go 1.21.1
+
+require github.com/jeevanandham-381/puppy v0.0.0-20230926081629-2cfafb588f20 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import ("fmt"
+"github.com/jeevanandham-381/puppy"
+)
+func main(){
+	fmt.Println("hii helo")
+	s:=puppy.Bark()
+	fmt.Println(s)
+}
